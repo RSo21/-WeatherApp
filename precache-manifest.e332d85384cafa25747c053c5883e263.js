61a1c1ba2766a33b07dff0755a5c3d279036e887
R <roksana_sobczak@wp.pl>
2020-05-03 17:52:23 +0200

    Updates

---
+++ precache-manifest.e332d85384cafa25747c053c5883e263.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f86b0efa35c11f6e1f6cb962cdeda7",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "90bbff81e65cf07c354f",
+    "url": "/WeatherApp/static/css/main.3cfd531f.chunk.css"
+  },
+  {
+    "revision": "929911f895e79b1f077f",
+    "url": "/WeatherApp/static/js/2.ba4828c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.ba4828c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90bbff81e65cf07c354f",
+    "url": "/WeatherApp/static/js/main.378ef2f7.chunk.js"
+  },
+  {
+    "revision": "a1be24b28b4289363924",
+    "url": "/WeatherApp/static/js/runtime-main.07d82fdf.js"
+  },
+  {
+    "revision": "432929e8185722e0076cae3fb2f8624f",
+    "url": "/WeatherApp/static/media/fabio-sasso-XjIajC-SYuc-unsplash.432929e8.jpg"
+  }
+]);
